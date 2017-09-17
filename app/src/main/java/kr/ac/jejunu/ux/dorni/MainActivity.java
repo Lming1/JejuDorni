927a198aa074cc8f7a3f233ce81ac54ce05c5e66
Lming1 <spear131121@gmail.com>
2017-09-17 13:26:13 +0900

    Info add

--- app/src/main/java/kr/ac/jejunu/ux/dorni/MainActivity.java
+++ app/src/main/java/kr/ac/jejunu/ux/dorni/MainActivity.java
@@ -174,13 +174,22 @@ public class MainActivity extends Activity {
 
     private void selectItem(int position) {
         // update the main content by replacing fragments
-        Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
+        if(position == 8)
+        {
+            Fragment fragment = new FirstFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        }
+        else
+        {
+            Fragment fragment = new PlanetFragment();
+            Bundle args = new Bundle();
+            args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+            fragment.setArguments(args);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        }
 
         // update selected item and title, then close the drawer
         mDrawerList.setItemChecked(position, true);
